2f8c0795c9a3dce580021460c3816e0ad2a1e7a4
Samuel Walker <services@sfxworks.net>
2018-12-27 00:13:45 -0500

    Init
    Init

---
+++ index.js
@@ -0,0 +1,9 @@
+addEventListener('fetch', event => {
+  event.respondWith(sendDate(event.request))
+})
+
+const _ = require("lodash")
+
+async function sendDate(request) {
+  return new Response(`${_.now()}`)
+}
